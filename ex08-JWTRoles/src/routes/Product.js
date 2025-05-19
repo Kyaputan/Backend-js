f97795e93e15868578f1fd8d2eff640d1da7f2b0
Rachata Singkhet <singkhet1@gmail.com>
2025-05-20 01:26:13 +0700

    Update Product.js

--- ex08-JWTRoles/src/routes/Product.js
+++ ex08-JWTRoles/src/routes/Product.js
@@ -9,25 +9,21 @@ const Product = async (req, res) => {
   if (!ID || !userRole) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-
   try {
-    const [results] = await pool.query(confirmUserQuery, [ID]);
-    if (results.length === 0) {
+
+    const [resultsUser] = await pool.query(confirmUserQuery, [ID]);
+    if (resultsUser.length === 0) {
       return res.status(401).json({ message: "User not found" });
-    }} catch (error) {
-    console.error("❌ Error executing query:", error);
-    return res.status(500).json({ error: "Database query failed" });
-  }
+    }
 
-  const query = "SELECT * FROM products";
-  try {
-    const [results] = await pool.query(query);
+    const queryProduct = "SELECT * FROM products";
+    const [resultsProducts] = await pool.query(queryProduct);
     res.json({
       message: "Our Products",
       email:   req.user.email,
       User_ID: req.user.userId,
       role:    req.user.role,
-      products: results.map((row) => ({
+      products: resultsProducts.map((row) => ({
         name:   row.product_name,
         price:  row.prices,
         stock:  row.stock,
